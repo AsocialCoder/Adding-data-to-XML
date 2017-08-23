b2dcca4b312f420bb12f2068237840899821c25b
AsocialCoder <cetinahmetemre@hotmail.com>
2017-08-23 16:23:27 +0300

    first commit

---
+++ tabukelimeprogram/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace tabukelimeprogram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.yasak5 = new System.Windows.Forms.TextBox();
+            this.yasak4 = new System.Windows.Forms.TextBox();
+            this.yasak3 = new System.Windows.Forms.TextBox();
+            this.yasak2 = new System.Windows.Forms.TextBox();
+            this.yasak1 = new System.Windows.Forms.TextBox();
+            this.kelime = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(279, 29);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(679, 541);
+            this.dataGridView1.TabIndex = 27;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 66);
+            this.button1.TabIndex = 26;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 322);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "yasak5";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "yasak4";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "yasak3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "yasak2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "yasak1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "kelime";
+            // 
+            // yasak5
+            // 
+            this.yasak5.Location = new System.Drawing.Point(84, 318);
+            this.yasak5.Name = "yasak5";
+            this.yasak5.Size = new System.Drawing.Size(122, 20);
+            this.yasak5.TabIndex = 19;
+            // 
+            // yasak4
+            // 
+            this.yasak4.Location = new System.Drawing.Point(84, 271);
+            this.yasak4.Name = "yasak4";
+            this.yasak4.Size = new System.Drawing.Size(122, 20);
+            this.yasak4.TabIndex = 18;
+            // 
+            // yasak3
+            // 
+            this.yasak3.Location = new System.Drawing.Point(84, 224);
+            this.yasak3.Name = "yasak3";
+            this.yasak3.Size = new System.Drawing.Size(122, 20);
+            this.yasak3.TabIndex = 17;
+            // 
+            // yasak2
+            // 
+            this.yasak2.Location = new System.Drawing.Point(84, 173);
+            this.yasak2.Name = "yasak2";
+            this.yasak2.Size = new System.Drawing.Size(122, 20);
+            this.yasak2.TabIndex = 16;
+            // 
+            // yasak1
+            // 
+            this.yasak1.Location = new System.Drawing.Point(84, 121);
+            this.yasak1.Name = "yasak1";
+            this.yasak1.Size = new System.Drawing.Size(122, 20);
+            this.yasak1.TabIndex = 15;
+            // 
+            // kelime
+            // 
+            this.kelime.Location = new System.Drawing.Point(84, 67);
+            this.kelime.Name = "kelime";
+            this.kelime.Size = new System.Drawing.Size(122, 20);
+            this.kelime.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 598);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.yasak5);
+            this.Controls.Add(this.yasak4);
+            this.Controls.Add(this.yasak3);
+            this.Controls.Add(this.yasak2);
+            this.Controls.Add(this.yasak1);
+            this.Controls.Add(this.kelime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox yasak5;
+        private System.Windows.Forms.TextBox yasak4;
+        private System.Windows.Forms.TextBox yasak3;
+        private System.Windows.Forms.TextBox yasak2;
+        private System.Windows.Forms.TextBox yasak1;
+        private System.Windows.Forms.TextBox kelime;
+
+    }
+}
+
---
+++ tabukelimeprogram/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+namespace tabukelimeprogram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void yukle()
+        {
+            XmlDocument i = new XmlDocument();
+            DataSet ds = new DataSet();
+            //xml dosyamızı okumak için bir reader oluşturuyoruz.
+            XmlReader xmlFile;
+            xmlFile = XmlReader.Create(@"veri.xml", new XmlReaderSettings());
+            //içeriği Dataset e aktarıyoruz.
+            ds.ReadXml(xmlFile);
+            //datagridviewin kaynağı olarak dataseti gösteriyoruz.
+            dataGridView1.DataSource = ds.Tables[0];
+            xmlFile.Close();
+        }
+
+       
+        private void button1_Click_1(object sender, EventArgs e)
+        {         
+               
+
+                XDocument x = XDocument.Load("veri.xml");
+
+                x.Element("grup").Add(
+                new XElement("kelimeler",
+                new XAttribute("kelime", kelime.Text),
+                new XAttribute("yasak1", yasak1.Text),
+                new XAttribute("yasak2", yasak2.Text),
+                new XAttribute("yasak3", yasak3.Text),
+                new XAttribute("yasak4", yasak4.Text),
+                new XAttribute("yasak5", yasak5.Text)
+                ));
+
+                x.Save(@"veri.xml");
+                yukle();
+                
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            yukle();
+        }
+    }
+}
